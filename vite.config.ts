853a42b7426cb3b4a7be65963a5bc624108d448d
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2025-03-24 00:13:58 +0900

    vite.config.tsを修正して@google-cloud/secret-managerモジュールを外部化

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			// サーバーサイドでのみ使用するモジュールを外部化
+			external: [
+				'@google-cloud/secret-manager'
+			]
+		}
+	}
 });
